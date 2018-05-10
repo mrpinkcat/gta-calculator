54df28fb955f4c64db9f6c02d5f0688b1c68f669
Gatien Anizan <gatien.anizan@gmail.com>
2018-05-10 19:41:10 +0200

    Initial Commit

---
+++ GTA-Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GTA-Calc
+//
+//  Created by Gatien ANIZAN on 10/05/2018.
+//  Copyright © 2018 Gatien ANIZAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
